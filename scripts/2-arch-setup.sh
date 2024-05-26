cdc1185c7e6d37656a2f7659e5621374029e82c7
anisbsalah <anisbensalah.89@gmail.com>
2024-05-26 06:32:52 +0100

    microcode already in hooks

--- scripts/2-arch-setup.sh
+++ scripts/2-arch-setup.sh
@@ -222,7 +222,6 @@ echo "
 # Initramfs
 sed -i 's/^MODULES=()/MODULES=(btrfs)/' /etc/mkinitcpio.conf
 sed -i 's/^BINARIES=()/BINARIES=(setfont)/' /etc/mkinitcpio.conf
-sed -i '/^HOOKS=/s/autodetect\( \|$\)/autodetect microcode\1/g' /etc/mkinitcpio.conf
 sed -i 's/^[#[:space:]]*COMPRESSION="zstd"/COMPRESSION="zstd"/' /etc/mkinitcpio.conf
 mkinitcpio -P
 
